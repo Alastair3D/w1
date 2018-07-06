de77ac46de0772495bd894696fa221de5dde7f8b
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-07-06 13:38:10 +0100

    14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,24 @@ require_relative 'bike'
 
 class DockingStation
 
+  def initialize
+    @bikes = []
+  end
+
 attr_reader :bike #this essentially writes: def bike/@bike/end and...
-#enables us to read the @bike attribute on any instance of the Bike class
+# enables us to read the @bike attribute on any instance of the Bike class
+# This 'attribute (instance variable) reader' gives us a RV of the instance var
+
+
   def release_bike
-    Bike.new
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
   # use an instance var to store the bike in the 'state' or this instance
-    @bike = bike
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
   end
-
-  # def bike
-  # # to give us a return value of the bike we docked
-  #   @bike
-  # end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,37 +5,50 @@ describe DockingStation do
   it 'responds to #release_bike' do
     expect(subject).to respond_to :release_bike
   end
-
   # one-liner syntax
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    # remember, nested describe blocks are used to describe
+    # behaviour specific to a particular method
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+    it 'raises an error when there are no bikes available' do
+      # this time, we don't want to dock a bike first obviously
+      # remember, subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
-  # it 'responds to #dock_bike' do
+    # it 'releases working bikes' do
+    #   bike = subject.release_bike
+    #   expect(bike).to be_working
+    # end
+
+  describe '#dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
+
+  # it 'responds to #dock(bike)' do
   #   expect(subject).to respond_to :dock(bike)
   # end
-
   # # one-liner syntax
   # it { is_expected.to respond_to :dock(bike) }
-
   it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it 'docks something' do
-    bike = Bike.new
-    # we want to return the bike we dock
-    expect(subject.dock(bike)).to eq bike
-  end
-
+  # it 'docks something' do
+  #   bike = Bike.new
+  #   # we want to return the bike we dock
+  #   expect(subject.dock(bike)).to eq bike
+  # end
   it { is_expected.to respond_to(:bike) }
 
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
 
 
 end

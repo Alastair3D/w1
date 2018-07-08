2ace25808b665f467a0c76d5ca860f1ea22e22cc
Alastair Edmonds <Alastair.Edmonds@gmail.com>
2018-07-08 15:09:56 +0100

    18, does not release broken bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,24 +2,42 @@ require_relative 'bike'
 
 class DockingStation
 
-  def initialize
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @capacity = capacity
     @bikes = []
   end
 
 attr_reader :bike #this essentially writes: def bike/@bike/end and...
 # enables us to read the @bike attribute on any instance of the Bike class
-# This 'attribute (instance variable) reader' gives us a RV of the instance var
+# This 'attribute (instance variable) reader' gives us a RV of the inst. var
 
+attr_reader :broken
+
+attr_accessor :capacity
+# this is a setter or writer i.e. a writeable
+
+DEFAULT_CAPACITY = 20
 
   def release_bike
     fail 'No bikes available' if @bikes.empty?
+    fail 'No working bikes available' if @broken = true
     @bikes.pop
   end
 
   def dock(bike)
   # use an instance var to store the bike in the 'state' or this instance
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if @bikes.count >= DEFAULT_CAPACITY
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,15 +11,22 @@ describe DockingStation do
   describe '#release_bike' do
     # remember, nested describe blocks are used to describe
     # behaviour specific to a particular method
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
-    end
+    # it 'releases a bike' do
+    #   bike = Bike.new
+    #   subject.dock(bike)
+    #   expect(subject.release_bike).to eq bike
+    # end
     it 'raises an error when there are no bikes available' do
       # this time, we don't want to dock a bike first obviously
       # remember, subject == DockingStation.new
       expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
+    it 'does not release broken bikes' do
+      b1 = Bike.new
+      b1.report_broken
+      subject.dock(b1)
+      expect { subject.release_bike }.to raise_error 'No working bikes available'
     end
   end
 
@@ -30,7 +37,10 @@ describe DockingStation do
 
   describe '#dock' do
     it 'raises an error when full' do
-      20.times { subject.dock(Bike.new) }
+      # 20.times { subject.dock(Bike.new) }
+        DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
+        # We are using :: as a 'Namespace operator'
+        # We use it to access the DEFAULT_CAPACITY constant
       expect { subject.dock Bike.new }.to raise_error 'Docking station full'
     end
   end
@@ -48,6 +58,18 @@ describe DockingStation do
   #   expect(subject.dock(bike)).to eq bike
   # end
   it { is_expected.to respond_to(:bike) }
+
+describe 'initialization' do
+  subject { DockingStation.new }
+  let(:bike) { Bike.new }
+
+  it 'defaults capacity' do
+   described_class::DEFAULT_CAPACITY.times do
+     subject.dock(bike)
+  end
+   expect{ subject.dock(bike) }.to raise_error 'Docking station full'
+ end
+end
 
 
 
